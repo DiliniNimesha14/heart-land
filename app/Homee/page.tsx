4966978b3b722eb6dc68a65e4d7a7402c730f5d4
knightowlrehan <rehan@knightowl.online>
2026-02-26 17:10:51 +0530

    test: build test

--- app/Homee/page.tsx
+++ app/Homee/page.tsx
@@ -1,27 +1,29 @@
 import type { Metadata } from "next";
-import Faq from '@/components/frequentlyAskedQuestions';
-import Initiative from '@/components/joinOurInitiatives';
-import HeroBanner from '@/components/makeThingsHappen';
-import Testimonials from '@/components/ourHappyCustomers';
-import Partners from '@/components/yourTrustedPartners';
-import HomeHero from '@/components/homeHero';
-import Blog from '@/components/blog';
+import Faq from "@/components/frequentlyAskedQuestions";
+import Initiative from "@/components/joinOurInitiatives";
+import HeroBanner from "@/components/makeThingsHappen";
+import Testimonials from "@/components/ourHappyCustomers";
+import Partners from "@/components/yourTrustedPartners";
+import HomeHero from "@/components/homeHero";
+import Blog from "@/components/blog";
 
 export const metadata: Metadata = {
   title: "Welcome",
-  description: "Welcome to Heartland General Trading - bringing authentic Sri Lankan flavors to the UAE for over 25 years. Quality products, trusted partnerships.",
+  description:
+    "Welcome to Heartland General Trading - bringing authentic Sri Lankan flavors to the UAE for over 25 years. Quality products, trusted partnerships.",
 };
 
 export default function HomePagee() {
   return (
     <>
-    <HomeHero />
-    <Partners />
-    <Blog/>
-     <Initiative /> 
-    {/* <Testimonials />  - hide for now */}  {/* test */} 
-    <Faq />
-    <HeroBanner />    
-    </> 
+      {/* for build*/}
+      <HomeHero />
+      <Partners />
+      <Blog />
+      <Initiative />
+      {/* <Testimonials />  - hide for now */} {/* test */}
+      <Faq />
+      <HeroBanner />
+    </>
   );
 }
